59a5c0d08d8c32fb4019476d932275804a9a876e
VB <vb.mail.247@gmail.com>
2014-06-06 11:31:28 +1000

    edited docs

--- doc/generate-html.sh
+++ doc/generate-html.sh
@@ -1,5 +1,5 @@
-export BOOST_ROOT=$HOME/dev/src/boost_1_55_0
+export BOOST_ROOT=$HOME/dev/boost_1_55_0
 
-bjam --toolset=gcc --enable-index
+$BOOST_ROOT/bjam --toolset=gcc --enable-index
 #bjam --toolset=gcc --enable-index pdf
 
