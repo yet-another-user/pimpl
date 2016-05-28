d33a5e9ca9de80dff65c1d6b8377750de67eff12
Vladimir Batov <vb.mail.247@gmail.com>
2016-05-28 20:51:11 +1000

    minor

--- include/pimpl.hpp
+++ include/pimpl.hpp
@@ -93,8 +93,8 @@ struct pimpl<user_type>::base
     using     pimpl_type = base;
     using   managed_type = manager<implementation>;
 
-    template<class T> using     no_ref = typename std::remove_reference<T>::type;
-    template<class T> using is_derived = typename std::enable_if<std::is_base_of<base, no_ref<T>>::value, null_type*>::type;
+    template<class T> using     rm_ref = typename std::remove_reference<T>::type;
+    template<class T> using is_derived = typename std::enable_if<std::is_base_of<base, rm_ref<T>>::value, null_type*>::type;
 
     bool         operator! () const { return !impl_.get(); }
     explicit operator bool () const { return  impl_.get(); }
