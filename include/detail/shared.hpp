84f9813cf7ae2b5ae4f356ccf97e9f5fed6d3a85
Mike Wazowski <vb.mail.247v@gmail.com>
2017-08-20 17:00:36 +1000

    playing with typename...

--- include/detail/shared.hpp
+++ include/detail/shared.hpp
@@ -1,24 +1,27 @@
 #ifndef IMPL_PTR_DETAIL_SHARED_HPP
 #define IMPL_PTR_DETAIL_SHARED_HPP
 
+#include "./first.hpp"
 #include <memory>
 
 namespace detail
 {
-    template<typename, typename =std::allocator<void>> struct shared;
+    template<typename, typename...> struct shared;
 }
 
-template<typename impl_type, typename allocator>
+template<typename impl_type, typename... more_types>
 struct detail::shared : std::shared_ptr<impl_type>
 {
-    using base_ref = std::shared_ptr<impl_type>&;
+    using   base_ref = std::shared_ptr<impl_type>&;
+    using alloc_type = typename std::conditional<
+                       sizeof...(more_types) == 0,
+                       std::allocator<impl_type>,
+                       typename first<more_types...>::type>::type;
 
     template<typename derived_type, typename... arg_types>
     void
     emplace(arg_types&&... args)
     {
-        using alloc_type = typename allocator::template rebind<derived_type>::other;
-
         base_ref(*this) = std::allocate_shared<derived_type>(alloc_type(), std::forward<arg_types>(args)...);
     }
 };
